34cdaf9cbc98cce43075551e939a285a81189cfb
dariamelinte <dariamelinte2003@gmail.com>
2019-06-22 12:45:36 +0300

    header

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+    
+    render(){
+        return(
+            <p className = "font container"> To Do List</p>
+        );
+    }
+};
+
+export default Header;
---
+++ src/container/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Aux from '../hoc/Auxiliary';
+import Header from '../components/header/Header';
+
+function App() {
+  return (
+    <Aux>
+      <Header />
+    </Aux>
+  );
+}
+
+export default App;
