7b4c5ecd8eb2074eb33b1f94a90c1a5e64456f05
dariamelinte <dariamelinte2003@gmail.com>
2019-07-19 14:05:51 +0300

    Updates

---
+++ precache-manifest.0126dc0916736965c035dcf0f7f51ac6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "944c50155b4bfa82654242d1c9b60897",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "225cb6799f1fd17d2fac",
+    "url": "/to-do-list/static/css/main.adaa5176.chunk.css"
+  },
+  {
+    "revision": "6bba650f3d4741e9f18d",
+    "url": "/to-do-list/static/js/2.839d71a6.chunk.js"
+  },
+  {
+    "revision": "225cb6799f1fd17d2fac",
+    "url": "/to-do-list/static/js/main.7b842f4a.chunk.js"
+  },
+  {
+    "revision": "15bb3101340a9b514097",
+    "url": "/to-do-list/static/js/runtime~main.9900d293.js"
+  }
+]);
