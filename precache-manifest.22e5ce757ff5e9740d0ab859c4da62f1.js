64ec2baeca3685208738d874c7baa1b498f278d3
dariamelinte <dariamelinte2003@gmail.com>
2019-07-19 16:50:32 +0300

    Updates

--- precache-manifest.22e5ce757ff5e9740d0ab859c4da62f1.js
+++ precache-manifest.22e5ce757ff5e9740d0ab859c4da62f1.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "107049a6c273f014c87863959910de35",
+    "revision": "ee3f0d2ce33c7d93ede2907704481032",
     "url": "/to-do-list/index.html"
   },
   {
-    "revision": "29db68317bf02e041e9e",
-    "url": "/to-do-list/static/css/main.b8b3199a.chunk.css"
+    "revision": "b298fb6a4d813b94f93f",
+    "url": "/to-do-list/static/css/main.330db9cd.chunk.css"
   },
   {
     "revision": "6bba650f3d4741e9f18d",
     "url": "/to-do-list/static/js/2.839d71a6.chunk.js"
   },
   {
-    "revision": "29db68317bf02e041e9e",
-    "url": "/to-do-list/static/js/main.7b842f4a.chunk.js"
+    "revision": "b298fb6a4d813b94f93f",
+    "url": "/to-do-list/static/js/main.a2505336.chunk.js"
   },
   {
     "revision": "15bb3101340a9b514097",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/to-do-list/precache-manifest.f4aa091c78d5a4dbb397ef8bf5a38114.js"
+  "/to-do-list/precache-manifest.22e5ce757ff5e9740d0ab859c4da62f1.js"
 );
 
 self.addEventListener('message', (event) => {
